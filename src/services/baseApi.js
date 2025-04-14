50056f3daf007c1fcf429fe0510ea5e8619334a1
Your Name <fadhishshaji@gmail.com>
2025-04-14 15:29:26 +0530

    env

--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -2,7 +2,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import { clearAuth, setCredentials } from '../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: import.meta.env.VITE_BASE_API,
     credentials: 'include', // For sending cookies (refresh token)
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
