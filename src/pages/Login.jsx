732c77ecbcfdb974739708bd707f797fadb03d61
Your Name <fadhishshaji@gmail.com>
2025-04-14 17:13:51 +0530

    Login password show

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,6 +11,7 @@ const Login = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const [loginUser, { isLoading }] = useLoginUserMutation();
   const [googleLogin] = useGoogleLoginMutation();
@@ -48,22 +49,41 @@ const Login = () => {
       <h2 style={{textAlign:'center'}}>Login</h2>
 
       <form onSubmit={handleSubmit}>
+      <div style={{display:'flex',width:'100%',flexDirection:'row',border:'1px solid #DDD',borderRadius:'8px',margin:'5px 0px'}}>
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
-          style={{ width: '100%', marginBottom: '10px' }}
-        />
+          style={{ width: '100%',border:'1px solid #DDD',borderRadius:'8px', padding:'5px',border:'none',outline:'none' }}
+          />
+          </div>
+         
+
+        <div style={{display:'flex',width:'100%',flexDirection:'row',border:'1px solid #DDD',borderRadius:'8px',margin:'5px 0px'}}>
+
         <input
-          type="password"
+          type={showPassword ? 'text' : 'password'}
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          style={{ width: '100%', marginBottom: '10px' }}
-        />
+          style={{ width: '100%', marginBottom: '10px',border:'none',outline:'none' }}
+          />
+        <button
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+            style={{
+               color: '#007bff',
+              cursor: 'pointer',
+              fontSize: '0.9rem',
+              padding: '15px 6px'
+            }}
+            >
+            {showPassword ? 'Hide' : 'Show'}
+          </button>
+            </div>
         <button type="submit" disabled={isLoading} style={{ width: '100%' }}>
           {isLoading ? 'Logging in...' : 'Login'}
         </button>
@@ -82,4 +102,3 @@ const Login = () => {
 };
 
 export default Login;
-1
